d4c767766dcb86411c1b20eb75142982b7187370
Павел Плющай <plyushhaj2@gmail.com>
2021-05-22 15:17:46 +0300

    Initialising VS Code for Unity Development

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        
+    }
+}
